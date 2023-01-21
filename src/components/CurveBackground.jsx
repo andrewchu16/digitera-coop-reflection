ac73e3821e48e26fd4d0a432dd9fab4116b64c56
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2023-01-21 00:41:01 -0500

    prefers reduced motion

--- src/components/CurveBackground.jsx
+++ src/components/CurveBackground.jsx
@@ -1,12 +1,12 @@
 function CurveBackground({ className = "fill-blue-400" }) {
     return (
-        <div className="absolute top-0 left-0 w-full overflow-hidden [line-height:0] animate-slide-down">
+        <div className="absolute top-0 left-0 w-full overflow-hidden [line-height:0] motion-safe:animate-slide-down">
             <svg
                 data-name="Layer 1"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 1200 120"
                 preserveAspectRatio="none"
-                className="relative block w-[164%] h-[40%] hover:w-[200%] hover:h-[60%] transition-[width,height] bg-transparent"
+                className="relative block w-[164%] h-[40%] motion-safe:hover:w-[200%] motion-safe:hover:h-[60%] transition-[width,height] bg-transparent"
             >
                 <path
                     d="M0,0V6c0,21.6,291,111.46,741,110.26,445.39,3.6,459-88.3,459-110.26V0Z"
