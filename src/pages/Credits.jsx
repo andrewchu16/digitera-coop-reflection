732cc1636409023c06c7185c26f8b3a3c77412c5
Andrew <67875282+Previouslynamedjeff@users.noreply.github.com>
2023-01-22 18:14:26 -0500

    updated home screen

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -11,7 +11,6 @@ function FreepikImageSource({ href, author }) {
     </li>
   );
 }
-
 function Credits() {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center bg-slate-300 md:px-96 md:py-48">
@@ -24,10 +23,35 @@ function Credits() {
       <div className="bg-neutral-100 w-full h-full flex flex-col items-center gap-4 p-8 rounded-lg">
         <h1 className="md:text-3xl text-2xl">Credits</h1>
         <ul className="list-disc md:text-xl text-lg">
-          <FreepikImageSource
-            href="https://www.freepik.com/free-vector/tiny-man-with-arrow-twisted-upward_16334792.htm"
-            author="pch.vector"
-          />
+          <li>
+            Image by{" "}
+            <a
+              href="https://www.freepik.com/free-vector/personal-growth-concept-illustration_36242177.htm"
+              className="text-sky-600 hover:underline"
+              target="_blank"
+            >
+              storyset
+            </a>{" "}
+            on Freepik
+          </li>
+          <li>
+            <a
+              target="_blank"
+              className="text-sky-600 hover:underline"
+              href="https://storyset.com/data"
+            >
+              Data illustrations by Storyset
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              className="text-sky-600 hover:underline"
+              href="https://storyset.com/people"
+            >
+              People illustrations by Storyset
+            </a>
+          </li>
         </ul>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,44 @@
 import { Link } from "react-router-dom";
 import CurveBackground from "../components/CurveBackground";
+import employabilitySkillsBg from "../assets/personal-growth.jpg";
+import softwareSkillsBg from "../assets/coding.svg";
+import overallExperienceBg from "../assets/thinking.svg";
 
-function LinkTile({ screen, children, text }) {
+function LinkTile({ screen, text, imgUrl }) {
   return (
     <Link
       to={screen}
-      className="bg-neutral-100 text-4xl z-10 flex-grow rounded-lg hover:scale-95 transition-transform duration-150 drop-shadow-md overflow-clip"
+      className="relative md:3xl bg-center bg-cover bg-origin-content text-2xl z-10 rounded-lg hover:scale-[.98] basis-full transition-transform duration-150 drop-shadow-md overflow-clip"
+      style={{ backgroundImage: `url(${imgUrl})` }}
     >
-      <div className="z-20 bg-neutral-100 hover:bg-neutral-100/50 hover:text-sky-500 w-full h-full flex justify-center items-center transition-colors">
+      <div
+        className={`absolute z-20 bg-neutral-100 hover:bg-neutral-100/60 hover:text-sky-500 w-full h-full flex justify-center items-center transition-colors`}
+      >
         <h1>{text}</h1>
       </div>
-      {children}
     </Link>
   );
 }
 
 function Home() {
   return (
-    <div className="bg-slate-200 w-screen h-screen flex md:flex-row flex-col justify-center items-stretch gap-8 md:p-20 p-8">
+    <div className="bg-slate-200 w-screen h-screen flex md:flex-row flex-col justify-center items-stretch gap-8 md:p-20 p-10">
       <CurveBackground className="fill-sky-600" />
       <LinkTile
         screen="/employability-skills"
         text="Employability Skills"
-      ></LinkTile>
-      <LinkTile screen="/software-skills" text="Software Skills" />
-      <LinkTile screen="/overall-experience" text="Overall Experience" />
+        imgUrl={employabilitySkillsBg}
+      />
+      <LinkTile
+        screen="/software-skills"
+        text="Software Skills"
+        imgUrl={softwareSkillsBg}
+      />
+      <LinkTile
+        screen="/overall-experience"
+        text="Overall Experience"
+        imgUrl={overallExperienceBg}
+      />
     </div>
   );
 }
